68d9e7a32375da10fc2888ae0dd67214d057e0bc
Christian MacMillan <christian.macmillan@b-reel.com>
2019-02-19 09:23:45 +0100

    Add vue component file

---
+++ BoxSwipe.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="box-swipe">
+        <p v-text="swipeObj" />
+    </div>
+</template>
+
+<script>
+    
+    import Swipe from "./Swipe.js"
+
+    export default {
+        name: "SwipeBox",
+        data() {
+            return {
+                swipeObj: {}
+            }
+        },
+        mounted() { 
+            this.init() 
+        },
+        methods: {
+            init() {
+                this.swipe = new Swipe({
+                    target: this.$el,
+                    callback: this.onSwipe
+                })
+                this.swipe.init()
+            },
+            onSwipe( swipe ) {
+                this.swipeObj = swipe
+                console.log( this.swipeObj )
+            }
+        },
+        beforeDestroy() {
+            this.swipe.destroy()
+        }
+    }
+
+</script>
+
+<style lang="scss" scoped>
+    
+    .box-swipe {
+        position: relative;
+        width: 250px;
+        height: 250px;
+        border: 1px solid lime;
+        p {
+            font-size: 14px;
+            color: lime;
+        }
+    }
+
+</style>
